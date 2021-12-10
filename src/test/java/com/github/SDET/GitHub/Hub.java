405d526baa93731aeb2c21658f814f04b9e73d69
lohith <lohith@Lohith>
2021-12-10 05:48:05 +0530

    committed 2

---
+++ src/test/java/com/github/SDET/GitHub/Hub.java
@@ -0,0 +1,13 @@
+package com.github.SDET.GitHub;
+
+import org.testng.annotations.Test;
+
+public class Hub {
+	
+	@Test
+	
+	public void storeData()
+	{
+		System.out.println("testing storage...");
+	}
+}
