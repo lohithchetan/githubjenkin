571da5e67f458514c50f3bd77e4e6994593c5f29
lohith <lohith@Lohith>
2021-12-15 12:49:44 +0530

    regression 2

---
+++ src/test/java/com/github/SDET/GitHub/Regression.java
@@ -0,0 +1,19 @@
+package com.github.SDET.GitHub;
+
+import org.testng.annotations.Test;
+
+public class Regression {
+	
+	@Test
+	
+	public void smokeTest() {
+		
+		int a=10;
+		int b=20;
+		int c=a-b;
+		System.out.println("sub ="  +c);
+		
+		
+	}
+
+}
