d804a4e330cfc6ca3d8af5ecc6dac78cf82c4724
lohith <lohith@Lohith>
2021-12-15 11:35:57 +0530

    smoke addded

---
+++ src/test/java/com/github/SDET/GitHub/SmokeTest.java
@@ -0,0 +1,19 @@
+package com.github.SDET.GitHub;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+	
+	@Test
+	
+	public void smokeTest() {
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("adding ="  +c);
+		
+		
+	}
+
+}
